3aa39a0f5b83c584ed85968b16f720dd8642be26
victor braz <victor.90braz@gmail.com>
2023-09-11 17:09:45 +0200

    apply style to nav bar

--- about.view.php
+++ about.view.php
@@ -17,7 +17,7 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+                <a href="/" class="text-gray-300 hover:bg-gray-700 px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
                 <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
                 <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
               </div>
@@ -118,7 +118,7 @@
 
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">ABOUT</h1>
       </div>
     </header>
 
