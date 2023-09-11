b5202c7fc770bfadee89c53f0968043e5b3a1c1c
victor braz <victor.90braz@gmail.com>
2023-09-11 18:19:46 +0200

    add banner to every page

--- views/about.view.php
+++ views/about.view.php
@@ -1,16 +1,11 @@
   <?php include __DIR__ . '/partials/head.php'; ?>
   <?php include __DIR__ . '/partials/nav.php'; ?>
+  <?php include __DIR__ . '/partials/banner.php'; ?>
 
-      <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">ABOUT</h1>
-        </div>
-      </header>
-
-      <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <p>about page</p>
-        </div>
-      </main>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <p>about page</p>
+    </div>
+  </main>
 
   <?php include __DIR__ . '/partials/footer.php'; ?>
