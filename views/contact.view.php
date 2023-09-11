3029b0e64795f7bf44b54da0661647ff47303417
victor braz <victor.90braz@gmail.com>
2023-09-11 18:15:36 +0200

    create header and footer

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dinamically Web Application</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="h-full">
-  <div class="min-h-full">
-
+    <?php include __DIR__ . '/partials/head.php'; ?>
     <?php include __DIR__ . '/partials/nav.php'; ?>
 
-    <header class="bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">CONTACT</h1>
-      </div>
-    </header>
+      <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900">CONTACT</h1>
+        </div>
+      </header>
 
-    <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>contact page</p>
-      </div>
-    </main>
-  </div>
-</body>
-</html>
+      <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <p>contact page</p>
+        </div>
+      </main>
+
+    <?php include __DIR__ . '/partials/footer.php'; ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dinamically Web Application</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="h-full">
-  <div class="min-h-full">
 
+  <?php include __DIR__ . '/partials/head.php'; ?>
   <?php include __DIR__ . '/partials/nav.php'; ?>
 
     <header class="bg-white shadow">
@@ -22,6 +13,5 @@
         <p>home page</p>
       </div>
     </main>
-  </div>
-</body>
-</html>
+
+  <?php include __DIR__ . '/partials/footer.php'; ?>
