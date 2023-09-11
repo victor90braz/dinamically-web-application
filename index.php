23d6618d8b7b7844936a8e750797841c8e1d3bbf
victor braz <victor.90braz@gmail.com>
2023-09-11 16:33:22 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$business = [
+  "name" => "laracast",
+  "cost" => 15,
+  "categories" => ["testing", "php", "javascript"]
+];
+
+function register () {
+  // create the user record in the club;
+  // log in them;
+  // send a welcome email;
+  // redirect to their new dashboard;
+}
+
+require "./index.view.php";
+
