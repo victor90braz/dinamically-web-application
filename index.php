44e113abfcb0855e52237db3e15f1c8e19e75243
victor braz <victor.90braz@gmail.com>
2023-09-12 18:11:56 +0200

    create a class person

--- index.php
+++ index.php
@@ -3,3 +3,39 @@
 include __DIR__ . "/router/router.php";
 
 
+class Person {
+  private $name;
+  private $age;
+
+  public function __construct($name, $age) {
+      $this->name = $name;
+      $this->age = $age;
+  }
+
+  public function getName() {
+      return $this->name;
+  }
+
+  public function getAge() {
+      return $this->age;
+  }
+
+  public function breathe() {
+      return "{$this->name} is breathing...";
+  }
+}
+
+// Create a Person object
+$victor = new Person("Victor", 32);
+
+// Get the person's age and breathing message
+$age = $victor->getAge();
+$breathingMessage = $victor->breathe();
+
+// Display information about the person
+echo "{$breathingMessage} Age: {$age}";
+
+
+
+
+
