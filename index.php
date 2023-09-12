7255b69fe8746c35201180bbbf4a1104393f816d
victor braz <victor.90braz@gmail.com>
2023-09-12 15:59:08 +0200

    create a router file

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
 <?php
 
-$url = parse_url($_SERVER['REQUEST_URI'])["path"];
-
-$routes = [
-    "/" => "./controllers/HomeController.php",
-    "/about" => "./controllers/AboutController.php",
-];
-
-routeToController($url, $routes);
-
-function abort() {
-    http_response_code(404);
-
-    require 'views/404.php';
-    die();
-}
-
-function routeToController($url, $routes) {
-
-    if (!array_key_exists($url, $routes)) {
-        abort();
-    }
-
-    require $routes[$url];
-
-}
+include __DIR__ . "/router/router.php";
 
 
