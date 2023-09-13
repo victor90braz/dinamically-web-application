00bad19c72a79e0f2cb7cb7084b13b1a5e6f9d0c
victor braz <victor.90braz@gmail.com>
2023-09-13 12:51:25 +0200

    clean up

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ include __DIR__ . "/router/router.php";
 include __DIR__ . "/data/DataBase.php";
 
 $config = require __DIR__ . "/config/config.php";
+
 $dataBase = new DataBase($config["dataBase"]);
+
 $posts = $dataBase->query("SELECT * from posts")->fetchAll();
 
 echo "<pre>";
