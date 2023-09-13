0e22a5e943682cf6c46fc5c476014d1482e03645
victor braz <victor.90braz@gmail.com>
2023-09-13 12:46:55 +0200

    remove hardcode from dabase

--- data/DataBase.php
+++ data/DataBase.php
@@ -4,11 +4,11 @@ class DataBase {
 
   public $connection;
 
-  public function __construct($config) {
+  public function __construct($config, $username = "root", $password = "") {
 
     $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-    $this->connection = new PDO($dsn, 'root', '', [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    $this->connection = new PDO($dsn, $username, $password, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
   }
 
   public function query ($query) {
