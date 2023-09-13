1e6768c9aa1651744287cfd5e62d62d15987887d
victor braz <victor.90braz@gmail.com>
2023-09-13 10:07:43 +0200

    fetch and fetchAll

--- index.php
+++ index.php
@@ -7,15 +7,13 @@ class DataBase {
 
   public $connection;
 
-  public function __construct()
-  {
+  public function __construct() {
     $dsn = "mysql:host=localhost;port=3306;dbname=laracast;user=root;charset=utf8mb4";
 
     $this->connection = new PDO($dsn);
   }
 
-  public function getQuery ($query)
-  {
+  public function getAllQueries ($query) {
 
     $statement = $this->connection->prepare($query);
     $statement->execute();
@@ -25,11 +23,22 @@ class DataBase {
 
     return $data;
   }
+
+  public function getQuery ($query) {
+
+    $statement = $this->connection->prepare($query);
+    $statement->execute();
+
+    $associativeArray = PDO::FETCH_ASSOC;
+    $data = $statement->fetch($associativeArray);
+
+    return $data;
+  }
 }
 
 $dataBase = new DataBase();
 $query = "SELECT * from posts";
-$posts = $dataBase->getQuery($query);
+$posts = $dataBase->getAllQueries($query);
 
 foreach($posts as $post)
 {
@@ -37,7 +46,18 @@ foreach($posts as $post)
 }
 
 echo "<pre>";
+echo "<h2>All queries</h2>";
 echo json_encode($posts, JSON_PRETTY_PRINT);
+echo "</pre>";
+
+$query = "SELECT * from posts where id=1";
+$post = $dataBase->getQuery($query);
+
+echo "<h2>One post</h2>";
+echo "<li>{$post['title']}</li>";
+
+echo "<pre>";
+echo json_encode($post, JSON_PRETTY_PRINT);
 echo "</pre>";
 
 
