d0a45060d76af8a88caddecbf7b0f937a1caa9b3
victor braz <victor.90braz@gmail.com>
2023-09-13 09:46:38 +0200

    refactor addind connection to a database class

--- index.php
+++ index.php
@@ -3,19 +3,28 @@
 include __DIR__ . "/router/router.php";
 
 
-$dsn = "mysql:host=localhost;port=3306;dbname=laracast;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
+class DataBase {
 
-$statement = $pdo->prepare("SELECT * from posts");
-$statement->execute();
+  public function query () {
+    $dsn = "mysql:host=localhost;port=3306;dbname=laracast;user=root;charset=utf8mb4";
+    $pdo = new PDO($dsn);
 
-$associativeArray = PDO::FETCH_ASSOC;
-$posts = $statement->fetchAll($associativeArray);
+    $statement = $pdo->prepare("SELECT * from posts");
+    $statement->execute();
+
+    $associativeArray = PDO::FETCH_ASSOC;
+    $posts = $statement->fetchAll($associativeArray);
+
+    return $posts;
+  }
+}
+
+$dataBase = new DataBase();
+$posts = $dataBase->query();
 
 foreach($posts as $post) {
   echo "<li>{$post['title']}</li>";
 }
-
 
 echo "<pre>";
 echo json_encode($posts, JSON_PRETTY_PRINT);
