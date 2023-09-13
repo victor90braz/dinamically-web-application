3488b565a2310fac687cb7d5696b586b60a5cb69
victor braz <victor.90braz@gmail.com>
2023-09-13 13:05:59 +0200

    rename folder to DataBase

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include __DIR__ . "/handleFunctions.php";
 include __DIR__ . "/router/router.php";
-include __DIR__ . "/data/DataBase.php";
+include __DIR__ . "/DataBase/DataBase.php";
 
 $config = require __DIR__ . "/config/config.php";
 
