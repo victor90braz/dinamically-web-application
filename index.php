e0812f5cdf3adabcff69d030a465d91a12e07276
victor braz <victor.90braz@gmail.com>
2023-09-13 10:11:22 +0200

    refactor again

--- index.php
+++ index.php
@@ -13,53 +13,22 @@ class DataBase {
     $this->connection = new PDO($dsn);
   }
 
-  public function getAllQueries ($query) {
+  public function query ($query) {
 
     $statement = $this->connection->prepare($query);
     $statement->execute();
 
-    $associativeArray = PDO::FETCH_ASSOC;
-    $data = $statement->fetchAll($associativeArray);
-
-    return $data;
+    return $statement;
   }
 
-  public function getQuery ($query) {
-
-    $statement = $this->connection->prepare($query);
-    $statement->execute();
-
-    $associativeArray = PDO::FETCH_ASSOC;
-    $data = $statement->fetch($associativeArray);
-
-    return $data;
-  }
 }
 
 $dataBase = new DataBase();
-$query = "SELECT * from posts";
-$posts = $dataBase->getAllQueries($query);
-
-foreach($posts as $post)
-{
-  echo "<li>{$post['title']}</li>";
-}
+$posts = $dataBase->query("SELECT * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<pre>";
-echo "<h2>All queries</h2>";
 echo json_encode($posts, JSON_PRETTY_PRINT);
 echo "</pre>";
-
-$query = "SELECT * from posts where id=1";
-$post = $dataBase->getQuery($query);
-
-echo "<h2>One post</h2>";
-echo "<li>{$post['title']}</li>";
-
-echo "<pre>";
-echo json_encode($post, JSON_PRETTY_PRINT);
-echo "</pre>";
-
 
 
 
